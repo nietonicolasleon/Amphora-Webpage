99e942562d21c364450993634d5886f5e3398750
Nicolás León Nieto Ferrari <nietonicolasleon@gmail.com>
2021-12-09 12:15:23 -0300

    Agregado y configurado ScrollReveal

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+ScrollReveal().reveal('.principal');
+ScrollReveal().reveal('.destacados', {delay : 500});
