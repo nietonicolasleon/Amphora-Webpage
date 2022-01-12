5ef87f8082a79f01a2925ae7feef63939edbb11f
Nicolás León Nieto Ferrari <nietonicolasleon@gmail.com>
2022-01-12 10:54:13 -0300

    Se creó acercade.php

---
+++ acercade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Conexión con Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+    <!--Fuente Playfair Display-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,400&display=swap" rel="stylesheet">
+    <!--Conexión CSS-->
+    <link rel="stylesheet" href="/estilos/styles.css"/>
+    <!--Conexión con ícono Personalizado-->
+    <link rel="shotcut icon" href="/images/Anforaico.ico"/>
+    <title>Acerca de Amphora</title>
+</head>
+<body>
+    <div class="menu-btn">
+        <i class="fas fa-bars"></i>
+    </div>
+    <!--Header-->
+    <div class="container">
+        <?php
+            require_once('recursos/header.php');
+        ?>
+        <hr/>
+        <h1 class="titular">¿Quiénes Somos?</h1>
+        <p class="parrafo">
+            Somos creyentes en el vino, porque "In Vino Veritas". Creemos que la siembra y la cosecha de las mejores uvas, el reposo en barricadas de roble, su diseño, añejamiento, embotellado y todo lo que hace del vino un elemento central para tantas culturas a lo largo de la historia, conformando el hermoso ritual del cuál somos parte. 
+            Creemos que una copa de un buen vino puede abrir la dimensión de un tiempo relajado, donde todos los sentidos se entregan a la experiencia de un degustar placentero.
+            Es por eso que en Amphora nos sentimos orgullosos de llevar nuestros vinos a tu casa. 
+            Sabemos que probándolos, te vas a convertir en un nuevo creyente, si todavía no lo sos. 
+            Creemos que, con un buen vino en nuestras copas, los buenos momentos se transforman en grandes,
+            y los grandes momentos se transforman en inolvidables, compartiéndolos con quienes queremos. 
+            Pero, por sobre todo, creemos que el simple acto de descorchar, la música de las copas llenándose, los intensos colores, los sugestivos aromas y los complejos sabores en nuestro paladar, transforman cualquier momento en algo mágico. 
+            Somos Amphora. Somos tu brindis. Somos tus inolvidables grandes momentos.
+        </p>
+        <h1 class="titular">¿Cómo puedo adquirir los productos?</h1>
+        <p class="parrafo">
+            Nos escribís a nuestras redes sociales y te los llevamos a donde nos indiques sin cargo dentro de CABA. 
+            También los podes pasar a buscar por nuestros puntos de entrega en Almagro, Palermo, Paternal o Monte Castro.
+            <h3 class="titular" id="titucontacto">Contactate con nosotros a través de:</h3>
+            <div class="mcontacto">
+                <div class="item1">
+                    <h5>Mensaje de Facebook:</h5>
+                    <a href="https://www.facebook.com/Amphora-102135655627341" target="_blank" id="facebig">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                </div>
+                <div class="item2">
+                    <h5>Mensaje de Instagram:</h5>
+                    <a href="https://www.instagram.com/vinoamphora/" target="_blank" id="instabig">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+                <div class="item3">
+                    <h5>Mensaje de WhatsApp:</h5>
+                    <a href="https://api.whatsapp.com/send?phone=+5491159592071" target="_blank" id="whatsbig">
+                        <i class="fab fa-whatsapp-square"></i>
+                    </a>
+                </div>
+                <div class="item4">
+                    <h5>Escríbanos un mail:</h5>
+                    <h5>vinoamphora@gmail.com</h5>
+                    <a href="mailto:vinoamphora@gmail.com" target="_blank" id="mailbig">
+                        <i class="fa fa-envelope"></i>
+                    </a>
+                </div>
+            </div>
+        </p>
+        <h1 class="titular">¿Cuáles son los medios de pago que puedo utilizar?</h1>
+        <p class="parrafo">
+            Efectivo, transferencia bancaria o Mercado Pago.
+        </p>
+        <h1 class="titular">¿Dónde puedo retirar los productos que adquirí?</h1>
+        <div class="mapas">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1952.2962612013641!2d-58.50113944930382!3d-34.620396005078995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc9d1bc15cf77%3A0x29f44559f7776318!2sMiranda%20%26%20Bernaldes%2C%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1640092055721!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1641.9610298409195!2d-58.46858338581296!3d-34.60613232601961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc9fe980c98b9%3A0xdcfa4c969302529b!2sTerrero%20%26%20Camarones%2C%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1640092170677!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1380.9899832455815!2d-58.41683785389604!3d-34.589840794461395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca8063519e27%3A0x4f29622b74c7799d!2sJer%C3%B3nimo%20Salguero%20%26%20General%20Lucio%20Norberto%20Mansilla%2C%20C1425%20CABA!5e0!3m2!1ses-419!2sar!4v1640092202836!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1641.85585216954!2d-58.42103658955438!3d-34.61145092969922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca5ef70a2611%3A0xe92549babeed836b!2sRivadavia%20%26%20Av.%20Medrano%2C%20C1178%20CABA!5e0!3m2!1ses-419!2sar!4v1640092272460!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+    <!--Redes Sociales-->
+    <?php
+        require_once('recursos/footer.php');
+    ?>
+    <!--Conexión con ScrollReveal-->
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <!--Conexión con el script principal del JavaScript-->
+    <script src="/main.js"></script>
+</body>
+</html>
