17642a593461925d097dab524a0996883de7e273
Nicolás León Nieto Ferrari <nietonicolasleon@gmail.com>
2022-01-12 10:42:00 -0300

    Se creó footer.php. Se integró este último en index.php

--- index.php
+++ index.php
@@ -69,27 +69,9 @@
         </div>
     </div>
     <!--Redes Sociales-->
-    <section class="redes">
-        <div class="footer-container">
-            <div class="links">
-                <h3>Seguinos en:</h3>
-                <a href="https://www.facebook.com/Amphora-102135655627341" target="_blank" id="face">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a href="https://www.instagram.com/vinoamphora/" target="_blank" id="insta">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <h3>Contáctenos por:</h3>
-                <a href="https://api.whatsapp.com/send?phone=+5491159592071" target="_blank" id="whats">
-                    <i class="fab fa-whatsapp-square"></i>
-                </a>
-                <a href="mailto:vinoamphora@gmail.com" target="_blank" id="mail">
-                    <i class="fa fa-envelope"></i>
-                </a>
-                <h4>Prohibida su venta a menores de 18 años. Beba con moderación.</h4>
-            </div>
-        </div>
-    </section>
+    <?php
+        require_once('recursos/footer.php');
+    ?>
     <!--Conexión con ScrollReveal-->
     <script src="https://unpkg.com/scrollreveal"></script>
     <!--Conexión con el script principal del JavaScript-->
