e7a2655778ac62599020c3992e577189cd60dc8b
Nicolás León Nieto Ferrari <nietonicolasleon@gmail.com>
2022-02-10 11:08:07 -0300

    Se corrigieron los href de vinos.php

--- productos/vinos.php
+++ productos/vinos.php
@@ -28,130 +28,130 @@
     </div>
     <div class="vinos">
         <div class="vino1">
-            <a href="../productos/vinos/csmrutini.php">
+            <a href="../vinos/csmrutini.php">
                 <img src="../images/vDiapositiva1.JPG" alt="Vino Rutini Cabernet/Malbec">
             </a>
-            <a href="../productos/vinos/csmrutini.php">
+            <a href="../vinos/csmrutini.php">
                 <h4>Vino Rutini Cabernet Sauvignon-Malbec</h4>
             </a>
         </div>
         <div class="vino2">
-            <a href="../productos/vinos/malbecrutini.php">
+            <a href="../vinos/malbecrutini.php">
                 <img src="../images/vDiapositiva2.JPG" alt="Vino Rutini Malbec">
             </a>
-            <a href="../productos/vinos/malbecrutini.php">
+            <a href="../vinos/malbecrutini.php">
                 <h4>Vino Rutini Malbec</h4>
             </a>
         </div>
         <div class="vino3">
-            <a href="../productos/vinos/malbecencuentro.php">
+            <a href="../vinos/malbecencuentro.php">
                 <img src="../images/vDiapositiva3.JPG" alt="Vino Encuentro Malbec">
             </a>
-            <a href="../productos/vinos/malbecencuentro.php">
+            <a href="../vinos/malbecencuentro.php">
                 <h4>Vino Encuentro Malbec</h4>
             </a>
         </div>
         <div class="vino4">
-            <a href="../productos/vinos/malbectrumpeter.php">
+            <a href="../vinos/malbectrumpeter.php">
                 <img src="../images/vDiapositiva4.JPG" alt="Vino Trumpeter Malbec">
             </a>
-            <a href="../productos/vinos/malbectrumpeter.php">
+            <a href="../vinos/malbectrumpeter.php">
                 <h4>Vino Trumpeter Malbec</h4>
             </a>
         </div>
         <div class="vino5">
-            <a href="../productos/vinos/malbecsanfelipe.php">
+            <a href="../vinos/malbecsanfelipe.php">
                 <img src="../images/vDiapositiva7.JPG" alt="Vino San Felipe">
             </a>
-            <a href="../productos/vinos/malbecsanfelipe.php">
+            <a href="../vinos/malbecsanfelipe.php">
                 <h4>Vino San Felipe Malbec</h4>
             </a>
         </div>
         <div class="vino6">
-            <a href="../productos/vinos/malbecencuentroestuche.php">
+            <a href="../vinos/malbecencuentroestuche.php">
                 <img src="../images/vDiapositiva8.JPG" alt="Vino Encuentro Malbec Estuche">
             </a>
-            <a href="../productos/vinos/malbecencuentroestuche.php">
+            <a href="../vinos/malbecencuentroestuche.php">
                 <h4>Vino Encuentro Malbec Estuche</h4>
             </a>
         </div>
         <div class="vino7">
-            <a href="../productos/vinos/blendtrumpeterreserve.php">
+            <a href="../vinos/blendtrumpeterreserve.php">
                 <img src="../images/vDiapositiva9.JPG" alt="Vino RB Estuche">
             </a>
-            <a href="../productos/vinos/blendtrumpeterreserve.php">
+            <a href="../vinos/blendtrumpeterreserve.php">
                 <h4>Vino Trumpeter Reserve Blend Estuche</h4>
             </a>
         </div>
         <div class="vino8">
-            <a href="../productos/vinos/malbecrutiniestuche.php">
+            <a href="../vinos/malbecrutiniestuche.php">
                 <img src="../images/vDiapositiva10.JPG" alt="Vino Rutini Malbec Estuche">
             </a>
-            <a href="../productos/vinos/malbecrutiniestuche.php">
+            <a href="../vinos/malbecrutiniestuche.php">
                 <h4>Vino Rutini Malbec Estuche</h4>
             </a>
         </div>
         <div class="vino9">
-            <a href="../productos/vinos/chardonnaytrumpeterreserve.php">
+            <a href="../vinos/chardonnaytrumpeterreserve.php">
                 <img src="../images/vDiapositiva11.JPG" alt="Vino Trumpeter Reserve Chardonnay">
             </a>
-            <a href="../productos/vinos/chardonnaytrumpeterreserve.php">
+            <a href="../vinos/chardonnaytrumpeterreserve.php">
                 <h4>Vino Trumpeter Reserve Chardonnay</h4>
             </a>
         </div>
         <div class="vino10">
-            <a href="../productos/vinos/malbecsyrahtrumpeter.php">
+            <a href="../vinos/malbecsyrahtrumpeter.php">
                 <img src="../images/vDiapositiva12.JPG" alt="Vino Trumpeter Malbec Syrah">
             </a>
-            <a href="../productos/vinos/malbecsyrahtrumpeter.php">
+            <a href="../vinos/malbecsyrahtrumpeter.php">
                 <h4>Vino Trumpeter Malbec Syrah</h4>
             </a>
         </div>
         <div class="vino11">
-            <a href="../productos/vinos/douxtrumpeter.php">
+            <a href="../vinos/douxtrumpeter.php">
                 <img src="../images/vDiapositiva13.JPG" alt="Vino Trumpeter Doux">
             </a>
-            <a href="../productos/vinos/douxtrumpeter.php">
+            <a href="../vinos/douxtrumpeter.php">
                 <h4>Vino Trumpeter Doux</h4>
             </a>
         </div>
         <div class="vino12">
-            <a href="../productos/vinos/pinotnoirtrumpeterreserve.php">
+            <a href="../vinos/pinotnoirtrumpeterreserve.php">
                 <img src="../images/vDiapositiva14.JPG" alt="Vino Trumpeter Reserve Pinot Noir">
             </a>
-            <a href="../productos/vinos/pinotnoirtrumpeterreserve.php">
+            <a href="../vinos/pinotnoirtrumpeterreserve.php">
                 <h4>Vino Trumpeter Reserve Pinot Noir</h4>
             </a>
         </div>
         <div class="vino13">
-            <a href="../productos/vinos/sauvignonblanctrumpeter.php">
+            <a href="../vinos/sauvignonblanctrumpeter.php">
                 <img src="../images/vDiapositiva15.JPG" alt="Vino Trumpeter Sauvignon Blanc">
             </a>
-            <a href="../productos/vinos/sauvignonblanctrumpeter.php">
+            <a href="../vinos/sauvignonblanctrumpeter.php">
                 <h4>Vino Trumpeter Sauvignon Blanc</h4>
             </a>
         </div>
         <div class="vino14">
-            <a href="../productos/vinos/malbecsanfelipeestuche.php">
+            <a href="../vinos/malbecsanfelipeestuche.php">
                 <img src="../images/vDiapositiva16.JPG" alt="Vino San Felipe Malbec Estuche">
             </a>
-            <a href="../productos/vinos/malbecsanfelipeestuche.php">
+            <a href="../vinos/malbecsanfelipeestuche.php">
                 <h4>Vino San Felipe Malbec Estuche</h4>
             </a>
         </div>
         <div class="vino15">
-            <a href="../productos/vinos/cssanfelipe.php">
+            <a href="../vinos/cssanfelipe.php">
                 <img src="../images/vDiapositiva17.JPG" alt="Vino San Felipe Cabernet Sauvignon">
             </a>
-            <a href="../productos/vinos/cssanfelipe.php">
+            <a href="../vinos/cssanfelipe.php">
                 <h4>Vino San Felipe Cabernet Sauvignon</h4>
             </a>
         </div>
         <div class="vino16">
-            <a href="../productos/vinos/malbectrumpeterestuche.php">
+            <a href="../vinos/malbectrumpeterestuche.php">
                 <img src="../images/vDiapositiva6.JPG" alt="Vino Trumpeter Malbec Estuche">
             </a>
-            <a href="../productos/vinos/malbectrumpeterestuche.php">
+            <a href="../vinos/malbectrumpeterestuche.php">
                 <h4>Vino Trumpeter Malbec Estuche</h4>
             </a>
         </div>
